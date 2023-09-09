853e547485cc24c34ac063b64ab57a2133f817ae
jaymhorsh <jaji.moshood97@yahoo.com>
2023-09-09 12:19:53 +0100

    Fix error(change to number)

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const DUMMY_EXPENSES = [
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = (expense) => {
+  const addExpenseHandler = (expense)=> {
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
---
+++ src/components/EditTracker/EditExpenses.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditExpenses = () => {
+  return (
+    <div>EditExpenses</div>
+  )
+}
+
+export default EditExpenses
